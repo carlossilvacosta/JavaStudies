73d59717adb7348a51c145d58190e11f02d8165b
Carlos Costa <carlossergiosilvacosta@gmail.com>
2023-10-03 14:35:11 -0300

    implementação da classe main

--- sistema_banco/src/application/Main.java
+++ sistema_banco/src/application/Main.java
@@ -1,10 +1,19 @@
 package application;
 
+import entities.Cliente;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		
+		Cliente cliente = new Cliente("Carlos", 23);
+		cliente.abrirConta("27121999");
+		cliente.getConta().depositar(1000);
+		cliente.getConta().exibirInformacoes();
+		cliente.getConta().sacar(300);
+		cliente.getConta().exibirInformacoes();
+		cliente.getConta().sacar(800);
+		cliente.getConta().exibirExtrato();
 
 	}
 
